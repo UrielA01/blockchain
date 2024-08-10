6213e6d04dbbbef67ba950ee0ccfdefac2bedadb
Uriel Azar <uriel.azar01@gmail.com>
2024-08-11 01:34:37 +0300

    add server

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,12 +1,20 @@
 import hashlib
 import json
 import time
+from urllib.parse import urlparse
+from uuid import uuid4
+
+from flask import Flask, jsonify, request
 
 
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.nodes = set()
+
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
 
     def new_block(self, proof, previous_hash=None):
         """
@@ -18,8 +26,8 @@ class Blockchain(object):
 
         block = {
             "index": len(self.chain) + 1,
-            "timestamp": time(),
-            "tansactions": self.current_transations,
+            "timestamp": time.time(),
+            "transactions": self.current_transactions,
             "proof": proof,
             "previous_hash": previous_hash or self.hash(self.chain[-1])
         }
@@ -61,7 +69,7 @@ class Blockchain(object):
 
         # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
         block_string = json.dumps(block, sort_keys=True).encode()
-        return hashlib.sha256(block_string).hexdiges()
+        return hashlib.sha256(block_string).hexdigest()
 
     def proof_of_work(self, last_proof):
         """
@@ -73,7 +81,7 @@ class Blockchain(object):
         """
 
         proof = 0
-        while self.valud_proof(last_proof, proof) is False:
+        while self.valid_proof(last_proof, proof) is False:
             proof += 1
 
         return proof
@@ -90,3 +98,80 @@ class Blockchain(object):
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
+
+    def register_node(self, address):
+        """
+        Add a new node to the list of nodes
+        :param address: <str> Address of node. Eg. 'http://192.168.0.5:5000'
+        :return: None
+        """
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+
+# Instantiate our node
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace("-", "")
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    # We run the proof of work algorithm to get the next proof
+    last_block = blockchain.last_block
+    last_proof = last_block["proof"]
+    proof = blockchain.proof_of_work(last_proof)
+
+    # We must receive a reward for finding the proof.
+    # The sender is "0" to signify that this node has mined a new coin.
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1
+    )
+
+    # Forge the new Block by adding it to the chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        "message": "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+
+
+@app.route("/transactions/new", methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+
+    # Check that the required fields are in the POST'ed data
+    required = ["sender", "sender", "amount"]
+    if not all(k in values for k in required):
+        return "Missing values", 400
+
+    # Create a new Transaction
+    index = blockchain.new_transaction(
+        values["sender"], values["recipient"], values["amount"])
+    response = {"message": f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+
+@app.route("/chain", methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
