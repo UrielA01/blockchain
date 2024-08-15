1300040add0d019342786e14d0347545bdd02be7
Uriel Azar <uriel.azar01@gmail.com>
2024-08-15 20:29:04 +0300

    refactor: Add `as_dict` method to Transaction class

--- src/blockchain_utils/transaction.py
+++ src/blockchain_utils/transaction.py
@@ -14,3 +14,14 @@ class Transaction:
 
     def valid_transaction(self) -> bool:
         return self.amount > 0 and self.sender and self.recipient
+
+    def as_dict(self):
+        return {
+            "sender": self.sender,
+            "recipient": self.recipient,
+            "amount": self.amount,
+            "timestamp": self.timestamp
+        }
+
+    def convert_to_bytes(self) -> bytes:
+        return str(self.as_dict()).encode('utf-8')
