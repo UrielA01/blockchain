3de97dd8ebf71ffbe7c515d93fe69bcad1a52ea4
Uriel Azar <uriel.azar01@gmail.com>
2024-08-15 20:58:45 +0300

    feat: Update Transaction class to include sender's Wallet object

--- src/blockchain_utils/transaction.py
+++ src/blockchain_utils/transaction.py
@@ -1,27 +1,36 @@
 from dataclasses import dataclass, field
 import time
 
+from utils.crypto_utils import calculate_sha256
+from wallet import Wallet
+
 
 @dataclass
 class Transaction:
-    sender: str
-    recipient: str
+    sender: Wallet
+    recipient: bytes
     amount: int
     timestamp: float = field(default_factory=lambda: time.time())
+    signature: str = None
 
     def __repr__(self):
-        return f"{self.sender} -> {self.recipient}: {self.amount}"
+        return f"{self.sender.address} -> {self.recipient}: {self.amount}"
 
     def valid_transaction(self) -> bool:
         return self.amount > 0 and self.sender and self.recipient
 
     def as_dict(self):
         return {
-            "sender": self.sender,
+            "sender": self.sender.address,
             "recipient": self.recipient,
             "amount": self.amount,
-            "timestamp": self.timestamp
+            "timestamp": self.timestamp,
+            "signature": self.signature
         }
 
     def convert_to_bytes(self) -> bytes:
         return str(self.as_dict()).encode('utf-8')
+
+    def __post_init__(self):
+        self.signature = Wallet.convert_signature_to_str(
+            self.sender.sign(self.convert_to_bytes()))
