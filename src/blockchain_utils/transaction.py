25156bd98eec99a0a11597221261f981e5aea241
Uriel Azar <uriel.azar01@gmail.com>
2024-08-16 01:36:44 +0300

    refactor: Update TransactionInput and TransactionOutput classes

--- src/blockchain_utils/transaction.py
+++ src/blockchain_utils/transaction.py
@@ -5,6 +5,37 @@ from utils.crypto_utils import calculate_sha256
 from wallet import Wallet
 
 
+class TransactionInput:
+    def __init__(self, transaction_hash: str, output_index: int, public_key: str = "", signature: str = ""):
+        self.transaction_hash = transaction_hash
+        self.output_index = output_index
+        self.public_key = public_key
+        self.signature = signature
+
+    def to_json(self, include_signature: bool = False) -> dict:
+        return {
+            "transaction_hash": self.transaction_hash,
+            "output_index": self.output_index,
+            "public_key": self.public_key,
+            "signature": self.signature if include_signature else ""
+        }
+
+    def __repr__(self):
+        return f"{self.transaction_hash}:{self.output_index}"
+
+
+class TransactionOutput:
+    def __init__(self, public_key_hash: str, amount: int):
+        self.amount = amount
+        self.public_key_hash = public_key_hash
+
+    def to_json(self) -> dict:
+        return {
+            "amount": self.amount,
+            "public_key_hash": self.public_key_hash
+        }
+
+
 @dataclass
 class Transaction:
     sender: Wallet
