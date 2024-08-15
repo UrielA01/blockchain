c079c97dbca7eab35109dc115ea791b86e8b9e11
Uriel Azar <uriel.azar01@gmail.com>
2024-08-15 20:59:57 +0300

    refactor: Update Wallet class to include sign and verify methods

--- src/wallet/wallet.py
+++ src/wallet/wallet.py
@@ -1,7 +1,9 @@
-from Crypto.Hash import RIPEMD160
-import hashlib
+import binascii
+from Crypto.Hash import RIPEMD160, SHA256
 from Crypto.PublicKey import RSA
+from Crypto.Signature import pkcs1_15
 import base58
+
 
 from utils.crypto_utils import calculate_ripemd160, calculate_sha256
 
@@ -11,6 +13,44 @@ class Wallet:
         self.private_key, self.public_key = initialize_wallet()
         self.address = base58.b58encode(
             hash_address(self.public_key))
+
+    def sign(self, transaction: bytes) -> bytes:
+        """
+        Sign a message with the wallet's private key.
+
+        :param message: The message to sign (in bytes).
+        :return: The digital signature (base64 encoded).
+        """
+        # Create a SHA-256 hash of the message
+        hash_obj = SHA256.new(transaction)
+
+        # Sign the hash using the private key
+        signature = pkcs1_15.new(self.private_key).sign(hash_obj)
+
+        return signature
+
+    @staticmethod
+    def convert_signature_to_str(signature: bytes) -> str:
+        return binascii.hexlify(signature).decode("utf-8")
+
+    def verify_signature(self, message: bytes, signature: bytes) -> bool:
+        """
+        Verify a digital signature using the wallet's public key.
+
+        :param message: The original message (in bytes).
+        :param signature: The signature to verify (in bytes).
+        :return: True if the signature is valid, False otherwise.
+        """
+        # Create a SHA-256 hash of the message
+        hash_obj = SHA256.new(message)
+
+        try:
+            # Verify the signature
+            pkcs1_15.new(RSA.import_key(self.public_key)
+                         ).verify(hash_obj, signature)
+            return True
+        except (ValueError, TypeError):
+            return False
 
 
 def initialize_wallet():
