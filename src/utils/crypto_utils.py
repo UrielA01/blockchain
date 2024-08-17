190955a4b7a7276ef4060b9339c1f9140785d75a
Uriel Azar <uriel.azar01@gmail.com>
2024-08-17 14:35:54 +0300

    refactor: Update calculate_sha256 and calculate_ripemd160 functions to accept both string and bytes input

--- src/utils/crypto_utils.py
+++ src/utils/crypto_utils.py
@@ -1,12 +1,15 @@
-import hashlib
-from Crypto.Hash import RIPEMD160
+from Crypto.Hash import RIPEMD160, SHA256
 
 
 def calculate_sha256(data: str | bytes) -> str:
-    return hashlib.sha256(data).hexdigest()
+    if type(data) == str:
+        data = bytearray(data, "utf-8")
+    hash = SHA256.new(data)
+    return hash.hexdigest()
 
 
-def calculate_ripemd160(data: str) -> str:
-    hash = RIPEMD160.new()
-    hash.update(data)
+def calculate_ripemd160(data: str | bytes) -> str:
+    if type(data) == str:
+        data = bytearray(data, "utf-8")
+    hash = RIPEMD160.new(data)
     return hash.hexdigest()
