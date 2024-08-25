5a6a742fc084f22674120bd94fbea84dcdc4d075
Uriel Azar <uriel.azar01@gmail.com>
2024-08-25 12:32:30 +0300

    refactor: convert mempool to json

---
+++ src/utils/io_mem_pool.py
@@ -0,0 +1,23 @@
+import json
+import os
+from typing import List
+
+from src.core.transaction import Transaction
+
+
+
+__file__ = os.path.abspath('../doc/mem_pool.json')
+def store_transactions_in_memory(transactions: List[dict]):
+    text = json.dumps(transactions, indent=4)
+    with open(__file__, "w") as file_obj:
+        file_obj.write(text)
+
+def get_transactions_from_memory() -> List[dict]:
+    with open(__file__, "r") as file_obj:
+        current_mem_pool_str = file_obj.read()
+        current_mem_pool_list = json.loads(current_mem_pool_str)
+    return current_mem_pool_list
+
+store_transactions_in_memory([{"transaction": "bla"}, {"transaction": "bla"}, {"transaction": "bla"}])
+print(get_transactions_from_memory())
+print(type(get_transactions_from_memory()))
